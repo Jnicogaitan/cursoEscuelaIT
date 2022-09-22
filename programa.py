0f34df85c1f963e7163b689137a9052929c897cf
Nicolas <nicolas.gaitan@grupor5.com>
2022-09-22 12:10:35 -0500

    modificando programa python

--- programa.py
+++ programa.py
@@ -1,3 +1,11 @@
+from audioop import findfactor
+from curses import def_prog_mode
+from fcntl import F_DUPFD, F_GET_SEALS
+from lib2to3.pgen2.pgen import DFAState
+from turtle import fd
+from xml.dom.minidom import getDOMImplementation
+
+
 fsgvdfgbdebedrbeb= respuesta
 print (respuesta)
 
@@ -5,3 +13,28 @@ print (respuesta)
 erjovholdfksvxfs
 xbv
 fdbv
+
+
+df
+globalsdgd
+F_GET_SEALSfd
+globaldf
+GLOBAL_FLAGS
+DFAState
+def_prog_modef
+
+f
+d
+
+d
+F_DUPFDg
+
+DFAStateg
+findfactorg
+
+finallygd
+
+getDOMImplementationf
+globaldf
+global
+
